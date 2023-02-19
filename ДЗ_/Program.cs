2260ae0cf99c9fd293e03d0095eb1ab58bd180e0
AnnaKiryukhina <124432443+AnnaKiryukhina@users.noreply.github.com>
2023-02-19 22:47:56 +0700

    Seminar 4 ДЗ задача 25  решение

---
+++ ДЗ_/Program.cs
@@ -0,0 +1,46 @@
+﻿// /Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+System.Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите степень возведения: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int Deg (int numberA)
+{
+    int deg = numberA;
+    for (int i = 2; i <= numberB; i++)
+    {
+        deg = deg*numberA; //
+    }
+    return deg;
+
+}
+
+System.Console.WriteLine($"число {numberA} в степени {numberB} = {Deg(numberA)}");//
+
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int Sum(int A)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= A; i++)
+//     {
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+// int number = ReadInt("Введите число А: ");
+// System.Console.WriteLine($"Сумма чисел от 1 до {number} = {Sum(number)}");
+
+// number = ReadInt("Введите число В: ");
+// System.Console.WriteLine($"Сумма чисел от 1 до {number} = {Sum(number)}");
+
+// number = ReadInt("Введите число С: ");
+// System.Console.WriteLine($"Сумма чисел от 1 до {number} = {Sum(number)}");
