0b598ccac2a0a4f43f2dd69fb877f51762d38d0f
AnnaKiryukhina <124432443+AnnaKiryukhina@users.noreply.github.com>
2023-02-20 13:08:19 +0700

    Seminar 4 ДЗ задача 29  решение

---
+++ ДЗ_Задача 29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+System.Console.WriteLine("Введите число элементов массива");
+int A = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] arr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0,100);
+    }
+}
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+int[] array = new int[A];
+FillArray(array);
+PrintArray(array);
+
+
